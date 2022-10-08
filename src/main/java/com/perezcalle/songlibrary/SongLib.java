084f082245ece7dc46cbca9376faa49545f97600
Julian O Calle <99502544+Jcalle13@users.noreply.github.com>
2022-10-08 13:38:30 -0400

    Update SongLib.java

--- src/main/java/com/perezcalle/songlibrary/SongLib.java
+++ src/main/java/com/perezcalle/songlibrary/SongLib.java
@@ -2,23 +2,34 @@ package com.perezcalle.songlibrary;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import application.MainSceneController;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
 
 
 public class SongLib extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(SongLib.class.getResource("MainScene.fxml"));
-        Scene appScene = new Scene(fxmlLoader.load(), 600, 674);
-        primaryStage.setResizable(false);
-        primaryStage.setScene(appScene);
-        primaryStage.setTitle("Song Library Application");
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/application/MainScene.fxml"));
+			AnchorPane rootLayout = (AnchorPane) loader.load(); 
+			MainSceneController controller = loader.getController();
+			controller.start(primaryStage);
+			Scene scene = new Scene(rootLayout, 600, 800);
+			
+			primaryStage.setTitle("Song library");
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.show();
+			stop();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
